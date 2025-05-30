ea83b963b2500105e90bacedece045ac86955996
Amruth Tetakali <156193119+amruth6002@users.noreply.github.com>
2025-05-30 13:21:35 +0530

    Update a.cpp

--- dp/a.cpp
+++ dp/a.cpp
@@ -3,27 +3,7 @@ using namespace std;
 #define int long long
 #define out(n) cout << n << "\n"
 const int inf = 1000000007;
-/*1.  (n<<i) multiplies n*2poweri
-2.  (n>>i) divides n/2poweri
-3.  (1<<i) 2 poweri (left_shift)
-4.  (1>>i) 1/2 poweri(right shft operator)
-5.  __builtin_popcountll(n); counts number of set bits in n(long long)
-6.  1e18 consists of 60 bits
-7.  n^(1<<i) sees if the ith bit of n is set or not
-8.  a.erase(unique(a.begin(),a.end()),a.end) removes the duplicates but have to sort a before that other-wise
-    some duplicates which are not side by side will be present
-9.*/
-// log function
-int lg(int n)
-{
-    int c = 0;
-    while (n > 1)
-    {
-        c++;
-        n = (n + 1) / 2;
-    }
-    return c;
-}
+
 void solve()
 {
 string a;cin>>a;
@@ -51,9 +31,9 @@ for(int i = 1; i <=n; i++) {
         }
     }
 }
-  out(dp[n][m]);  
-    
+out(dp[n][m]);    
 }
+
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
